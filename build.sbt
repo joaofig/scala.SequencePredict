80278395ced522544c327fefe8f3b471b7876b27
João Paulo Figueira <joao.figueira@frotcom.com>
2018-06-09 14:59:33 +0100

    First commit
    Very first commit using only OS tools

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "SequencePredict"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
---
+++ src/main/scala/CompactPredictionTree.scala
@@ -0,0 +1,13 @@
+class CompactPredictionTree[T](val item: T, val parent: CompactPredictionTree[T]) {
+  var children: List[CompactPredictionTree[T]] = List()
+  val support: Int = 0
+
+  def this(nodeItem: T) {
+    this(nodeItem, this)
+  }
+
+  def addChild(newItem: T): Unit = {
+    val node = new CompactPredictionTree[T](newItem)
+    children = node :: children
+  }
+}
