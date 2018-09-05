d96920c65ea64d366e8de032a43fe9a75e401cfe
João Paulo Figueira <joao.figueira@frotcom.com>
2018-09-05 16:43:03 +0100

    Inverted index add

--- src/main/scala/CPT/InvertedIndex.scala
+++ src/main/scala/CPT/InvertedIndex.scala
@@ -1,5 +1,15 @@
 package CPT
+import scala.collection.mutable
 
 class InvertedIndex[T] {
+  private val index: mutable.HashMap[T, mutable.HashSet[Int]] = mutable.HashMap.empty
 
+  private def add(item: T, sequenceId: Int) : Unit = {
+    if(!index.contains(item))
+      index.put(item, mutable.HashSet.empty)
+    index(item).add(sequenceId)
+  }
+
+  def containsKey(key: T): Boolean = index.contains(key)
+  def get(key: T): Option[mutable.HashSet[Int]] = index.get(key)
 }
